25d22d2ece42425f41b4cc4a78cb4b348a4cb918
xb <717674114@qq.com>
2017-03-13 19:59:03 +0800

    change allow_origin

--- src/main/java/com/nirvana/app/interceptor/AjaxInterceptor.java
+++ src/main/java/com/nirvana/app/interceptor/AjaxInterceptor.java
@@ -15,10 +15,10 @@ public class AjaxInterceptor extends HandlerInterceptorAdapter {
 		System.out.println("userId:"+userId);
 		request.setAttribute("userId", userId);
 		//调试域
-		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+		//response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
 		//运行域
 		//response.setHeader("Access-Control-Allow-Origin", "http://139.199.76.64");
-		//response.setHeader("Access-Control-Allow-Origin", "http://mh100.com.cn");
+		response.setHeader("Access-Control-Allow-Origin", "http://mh100.com.cn");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
