dd7886f7cb7ef0dd6fc5c0ca28b981c0e5fc4bd4
xb <717674114@qq.com>
2017-03-13 13:30:58 +0800

    update doc and note

--- src/main/java/com/nirvana/app/interceptor/AjaxInterceptor.java
+++ src/main/java/com/nirvana/app/interceptor/AjaxInterceptor.java
@@ -11,13 +11,12 @@ public class AjaxInterceptor extends HandlerInterceptorAdapter {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
-//		request.getSession().setAttribute("userid", 1);
-		//request.setAttribute("userid", 1);
 		Integer userId=(Integer)request.getSession().getAttribute("userid");
 		System.out.println("userId:"+userId);
 		request.setAttribute("userId", userId);
-		//response.setHeader("Access-Control-Allow-Origin", "*");
+		//调试域
 		response.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
+		//运行域
 		//response.setHeader("Access-Control-Allow-Origin", "http://139.199.76.64");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
